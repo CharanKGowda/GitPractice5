517e9f311e20c41e6446883325a881f6c0a00c6c
Charan K Gowda <Charan.Gowda@mindtree.com>
2022-06-30 14:47:10 +0530

    Modified in Folder 2

--- jdbc1.java
+++ jdbc1.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 import com.mysql.cj.protocol.Resultset;
-
+//Just typing something to modify
 public class jdbc1 {
 
 	public static void main(String[] args) throws SQLException{
